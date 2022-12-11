23e1529dde145f1c9770ad9a072a303b4dc6003b
Paulo Henrique Andrade Alves <80326186+PhAlves23@users.noreply.github.com>
2022-12-11 17:45:36 -0300

    :sparkles: create Card component

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+export default function Card() {
+  return (
+    <div className="card">
+      <h2>Brigadeiro Gourmet</h2>
+      <span>R$ 5,00</span>
+      <div className="card-info">
+        <div className="card-info-detail">
+          <p>Brigaderia do seu Zé</p>
+        </div>
+        <div className="card-info-detail">
+          <p>11 9999-9999</p>
+        </div>
+      </div>
+    </div>
+  );
+}
